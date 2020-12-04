c3e8c2fd39ab8bbac3e9793123f7e641e3574d6f
adel-haouas <adel.haouas@gmail.com>
2020-12-04 23:39:13 +0100

    Update tn.cidr.v4.sh

--- tn.cidr.v4.sh
+++ tn.cidr.v4.sh
@@ -10,17 +10,24 @@ lastestFile=`curl -ls ftp://ftp.afrinic.net/pub/stats/afrinic/$cYear/|egrep "del
 curl -s ftp://ftp.afrinic.net/pub/stats/afrinic/$cYear/$lastestFile > $lastestFile
 
 for i in $(curl -s https://raw.githubusercontent.com/herrbischoff/country-ip-blocks/master/ipv4/tn.cidr|sort -V);do
- echo -en $i"\t"
+ #echo -en $i"\t"
  ii=$(echo $i|awk -F'/' '{print $1}')
  
  assigmentDate=$(cat /tmp/$lastestFile|egrep $ii|awk -F'|' '{print $6}')
  assigmentDate_ts=$(date -d `echo $assigmentDate` '+%s')
  diff_days=$(( ( cDate_ts - assigmentDate_ts )/(60*60*24) ))
 
+ #if [ $diff_days -lt 60 ]; then
+ # echo -en "\e[31m"$assigmentDate"\e[0m""\t"
+ #else
+ # echo -en $assigmentDate"\t"
+ #fi
+ LIR=$(whois -h whois.afrinic.net $i |egrep netname:|awk -F":" '{print $2}'| sed -e 's/^[[:space:]]*//'| sed -e 's/[[:space:]]*$//')
+ 
+ #echo -en $i"\t"$assigmentDate"\t"$LIR
  if [ $diff_days -lt 60 ]; then
-  echo -en "\e[31m"$assigmentDate"\e[0m""\t"
+  echo -en $i"\t"$assigmentDate"\t"$LIR" \e[1;31;42m*\e[0m"|column -t
  else
-  echo -en $assigmentDate"\t"
+  echo -en $i"\t"$assigmentDate"\t"$LIR|column -t
  fi
- whois -h whois.afrinic.net $i |egrep netname:|awk -F":" '{print $2}'| sed -e 's/^[[:space:]]*//'| sed -e 's/[[:space:]]*$//'
-done|column -t
+done
