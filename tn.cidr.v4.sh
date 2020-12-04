bb650a771bd79c1abe166662958a426d4e9e695b
adel-haouas <adel.haouas@gmail.com>
2020-12-04 23:12:11 +0100

    Update tn.cidr.v4.sh

--- tn.cidr.v4.sh
+++ tn.cidr.v4.sh
@@ -22,24 +22,4 @@ for i in $(curl -s https://raw.githubusercontent.com/herrbischoff/country-ip-blo
   echo -en $assigmentDate"\t"
  fi
  whois -h whois.afrinic.net $i |egrep netname:|awk -F":" '{print $2}'| sed -e 's/^[[:space:]]*//'| sed -e 's/[[:space:]]*$//'
-done|column -t
-
-
-
-
-
-
-
-
-#clear && cd /tmp
-#cYear=$(date +"%Y")
-#lastestFile=`curl -ls ftp://ftp.afrinic.net/pub/stats/afrinic/$cYear/|egrep "delegated-afrinic-$cYear"|egrep -v ".asc$|.md5$"|tail -1`
-
-#curl -s ftp://ftp.afrinic.net/pub/stats/afrinic/$cYear/$lastestFile > $lastestFile
-
-#for i in $(curl -s https://raw.githubusercontent.com/herrbischoff/country-ip-blocks/master/ipv4/tn.cidr|sort -V);do
-# echo -en $i"\t"
-# ii=$(echo $i|awk -F'/' '{print $1}')
-# echo -en $(cat /tmp/$lastestFile|egrep $ii|awk -F'|' '{print $6}')"\t"
-# whois -h whois.afrinic.net $i |egrep netname:|awk -F":" '{print $2}'| sed -e 's/^[[:space:]]*//'| sed -e 's/[[:space:]]*$//'
-#done|column -t
+done
